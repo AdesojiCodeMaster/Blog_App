fe9265ca76b55f8e49e55fba3dd82c7af6f9188e
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-09-24 17:26:43 +0100

    Complete a few gems iteration

--- blog/blogger/app/models/article.rb
+++ blog/blogger/app/models/article.rb
@@ -1,7 +1,10 @@
 class Article < ApplicationRecord
-  has_many :comments
-  has_many :taggings , dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
 
   def tag_list
     self.tags.collect do |tag|
