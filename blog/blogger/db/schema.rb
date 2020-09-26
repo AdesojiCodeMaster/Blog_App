09bb9c11f5f47a04f959af7330b7e3572da7319e
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-09-26 17:55:55 +0100

    Add department name in schema.rb

--- blog/blogger/db/schema.rb
+++ blog/blogger/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_202027) do # rubocop:disable Metrics/BlockLength
+ActiveRecord::Schema.define(version: 2020_09_24_202027) do # rubocop:disable Metrics/BlockLength, Style/NumericLiterals
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'body'
