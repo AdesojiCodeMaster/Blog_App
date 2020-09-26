7de0840227945c7f883e63740593f323130c0586
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-09-26 17:12:54 +0100

    Correct schema.rb

--- blog/blogger/db/schema.rb
+++ blog/blogger/db/schema.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Metrics/BlockLength
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_924_202_027) do
+ActiveRecord::Schema.define(version: 2020_09_24_202027) do # rubocop:disable Metrics/BlockLength
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'body'
@@ -61,4 +60,4 @@ ActiveRecord::Schema.define(version: 20_200_924_202_027) do
   add_foreign_key 'taggings', 'articles'
   add_foreign_key 'taggings', 'tags'
 end
-# rubocop:enable Metrics/BlockLength
+
